e14b3a76063b711e46512f7fce0c9503df3c192b
Ilan Perer <ilan.perez@epfl.ch>
2021-10-15 10:51:23 +0200

    break point analysis with histograms

--- BreakPoint.R
+++
@@ -1,6 +0,0 @@
-tennis2021 <- read.csv("C:/Users/ilanp/Documents/EPFL/Mathématiques/MA master sem 3 automne 2021/Statistical Computation and Visualization/atp_matches_2021.csv")
-tennis2019 <- read.csv("C:/Users/ilanp/Documents/EPFL/Mathématiques/MA master sem 3 automne 2021/Statistical Computation and Visualization/tennis_atp-master/atp_matches_2019.csv")
-
-winnerFacedBreakPoint <- tennis2019$w_bpFaced
-winnerSavedBreakPoint <- tennis2019$w_bpSaved
-winnerRatioSavedBreakPoint <- winnerSavedBreakPoint / winnerFacedBreakPoint
